d22da80e3dd8e39a61bdec7fa2c1d435148cc650
Oleksandr Dubenko <oldubenko@microsoft.com>
2026-03-20 10:14:36 -0400

    frontend: EditorDialog: Ensure editor is visible on high zoom by providing proper scrolling

--- frontend/src/components/common/Resource/EditorDialog.tsx
+++ frontend/src/components/common/Resource/EditorDialog.tsx
@@ -478,8 +478,9 @@ export default function EditorDialog(props: EditorDialogProps) {
       {uploadFiles ? <UploadDialog setUploadFiles={setUploadFiles} setCode={setCode} /> : ''}
       <DialogContent
         sx={{
+          minHeight: '400px',
           height: '80%',
-          overflowY: 'hidden',
+          overflowY: 'auto',
           display: 'flex',
           flexDirection: 'column',
         }}
